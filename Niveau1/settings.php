91a28ff4d77b0e1e147cfaaf2294c23b5352ab7a
nathanc8 <cazard.nathan@gmail.com>
2024-10-07 14:23:31 +0200

    settings ok, mais pourquoi ?

--- Niveau1/settings.php
+++ Niveau1/settings.php
@@ -59,15 +59,15 @@
              * Etape 3: récupérer le nom de l'utilisateur
              */
             $laQuestionEnSql = "
-                    SELECT users.*, 
-                    count(DISTINCT posts.id) as totalpost, 
-                    count(DISTINCT given.post_id) as totalgiven, 
-                    count(DISTINCT received.user_id) as totalreceived 
-                    FROM users 
-                    LEFT JOIN posts ON posts.user_id=users.id 
-                    LEFT JOIN likes as given ON given.user_id=users.id 
-                    LEFT JOIN likes as received ON received.post_id=posts.id 
-                    -- WHERE users.id = $userId 
+                    SELECT users.*,
+                    count(DISTINCT posts.id) as totalpost,
+                    count(DISTINCT given.post_id) as totalgiven,
+                    count(DISTINCT received.user_id) as totalreceived
+                    FROM users
+                    LEFT JOIN posts ON posts.user_id=users.id
+                    LEFT JOIN likes as given ON given.user_id=users.id
+                    LEFT JOIN likes as received ON received.post_id=posts.id
+                    WHERE users.id = '$userId'
                     GROUP BY users.id
                     ";
             $lesInformations = $mysqli->query($laQuestionEnSql);
