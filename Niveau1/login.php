9fef253f9abccf0d26d3758b188075443544f029
Your Name <you@example.com>
2024-10-09 16:39:14 +0200

    création branch

--- Niveau1/login.php
+++ Niveau1/login.php
@@ -32,11 +32,12 @@ $pageTitle = 'login';
                 // Etape 1 : vérifier si on est en train d'afficher ou de traiter le formulaire
                 // si on recoit un champs email rempli il y a une chance que ce soit un traitement
                 $enCoursDeTraitement = isset($_POST['email']);
+
                 if ($enCoursDeTraitement) {
                     // on ne fait ce qui suit que si un formulaire a été soumis.
                     // Etape 2: récupérer ce qu'il y a dans le formulaire @todo: c'est là que votre travail se situe
                     // observez le résultat de cette ligne de débug (vous l'effacerez ensuite)
-                    echo "<pre>" . print_r($_POST, 1) . "</pre>";
+                    // echo "<pre>" . print_r($_POST, 1) . "</pre>";
                     // et complétez le code ci dessous en remplaçant les ???
                     $emailAVerifier = $_POST['email'];
                     $passwdAVerifier = $_POST['motpasse'];
@@ -49,7 +50,7 @@ $pageTitle = 'login';
                     $emailAVerifier = $mysqli->real_escape_string($emailAVerifier);
                     $passwdAVerifier = $mysqli->real_escape_string($passwdAVerifier);
                     // on crypte le mot de passe pour éviter d'exposer notre utilisatrice en cas d'intrusion dans nos systèmes
-                    $passwdAVerifier = md5($passwdAVerifier);
+                    $passwdAVerifier = password_hash($passwdAVerifier, PASSWORD_DEFAULT);
                     // NB: md5 est pédagogique mais n'est pas recommandée pour une vraies sécurité
                     //Etape 5 : construction de la requete
                     $lInstructionSql = "SELECT * "
@@ -59,7 +60,7 @@ $pageTitle = 'login';
                     // Etape 6: Vérification de l'utilisateur
                     $res = $mysqli->query($lInstructionSql);
                     $user = $res->fetch_assoc();
-                    if (! $user or $user["password"] != $passwdAVerifier) {
+                    if (!$user or $user["password"] != $passwdAVerifier) {
                         echo "La connexion a échouée. ";
                     } else {
                         echo "Votre connexion est un succès : " . $user['alias'] . ".";
