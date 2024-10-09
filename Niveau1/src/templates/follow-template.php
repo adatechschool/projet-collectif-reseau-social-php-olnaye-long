de8ef4049240a9afcdc1ea5a3195c7d3dbb17616
Your Name <you@example.com>
2024-10-09 14:43:39 +0200

    usuer link added in follow-template

--- Niveau1/src/templates/follow-template.php
+++ Niveau1/src/templates/follow-template.php
@@ -2,8 +2,11 @@
 while ($follow = $lesInformations->fetch_assoc()) {
     ?>
     <article>
+    <!-- <?php //echo "<pre>" . print_r($follow, 1) . "</pre>"?> -->
+
         <img src="./src/img/user.jpg" alt="blason" />
-        <h3><?php echo $follow['alias'] ?></h3>
+        <a href="./wall.php?user_id=<?php echo $follow['id'] ?>"> <h3><?php echo $follow['alias'] ?></h3> </a>
         <p>id:<?php echo $follow['id'] ?></p>
     </article>
 <?php } ?>
+
