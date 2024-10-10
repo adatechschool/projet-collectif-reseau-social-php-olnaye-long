33ade9fee2942bb9b42d29f5df5cc705e620fd0a
nathanc8 <cazard.nathan@gmail.com>
2024-10-10 16:51:36 +0200

    on a bien bossé ! session dans la page follow, qui permet de faire en sorte que la fonction de follow soit applicable à tous les users

--- Niveau1/src/methods/follow.php
+++ Niveau1/src/methods/follow.php
@@ -1,38 +1,45 @@
-<?php if ($pageTitle == 'wall') {
-    // $connectedId = $connected_id;
-    //Vérification que l'user connecté suit ou non la personne
-    $verificationFollow = $mysqli->query("
+<?php
+session_start();
+
+if (!isset($_SESSION['connected_id'])) {
+    header("Location: login.php");
+} else {
+
+    if ($pageTitle == 'wall') {
+        //Vérification que l'user connecté suit ou non la personne
+        $sessionId = $_SESSION['connected_id'];
+        $verificationFollow = $mysqli->query("
             SELECT * FROM followers
-            WHERE followed_user_id = '5'
-            AND following_user_id = '4'
-            "); //followed_user_id = '$userId' following_user_id = 'sessionId'
+            WHERE followed_user_id = '$userId'
+            AND following_user_id = '$sessionId'
+            ");
 
-    if ($verificationFollow->num_rows == 0) {
-        $isFollowing = false;
-        $follow_button_label = "Suivre";
-    } else {
-        $isFollowing = true;
-        $follow_button_label = "Ne pas suivre";
-    }
-
-    if (isset($_POST['followButton'])) {
         if ($verificationFollow->num_rows == 0) {
-            $follow = $mysqli->query(
-                "INSERT INTO followers(followed_user_id, following_user_id) VALUES ('5', '4')"
-            ); //followed_user_id = '$userId' following_user_id = 'sessionId'
-            $isFollowing = !$isFollowing;
-            $follow_button_label = "Ne pas suivre";
-        } else {
-            $unfollow = $mysqli->query(
-                "DELETE FROM followers WHERE followed_user_id = '5' AND following_user_id = '4'"
-            ); //followed_user_id = '$userId' following_user_id = 'sessionId'
-            $isFollowing = !$isFollowing;
+            $isFollowing = false;
             $follow_button_label = "Suivre";
+        } else {
+            $isFollowing = true;
+            $follow_button_label = "Ne pas suivre";
         }
-    }
-?>
-    <form action="" method='post'>
-        <input type="hidden" name="isFollowing" value="<?php echo $isFollowing; ?>">
-        <input type="submit" value="<?php echo $follow_button_label ?>" name="followButton">
-    </form>
+
+        if (isset($_POST['followButton'])) {
+            if ($verificationFollow->num_rows == 0) {
+                $follow = $mysqli->query(
+                    "INSERT INTO followers(followed_user_id, following_user_id) VALUES ('$userId', '$sessionId')"
+                );
+                $isFollowing = !$isFollowing;
+                $follow_button_label = "Ne pas suivre";
+            } else {
+                $unfollow = $mysqli->query(
+                    "DELETE FROM followers WHERE followed_user_id = '5' AND following_user_id = '4'"
+                );
+                $isFollowing = !$isFollowing;
+                $follow_button_label = "Suivre";
+            }
+        } ?>
+        <form action="" method='post'>
+            <input type="hidden" name="isFollowing" value="<?php echo $isFollowing; ?>">
+            <input type="submit" value="<?php echo $follow_button_label ?>" name="followButton">
+        </form>
+    <?php } ?>
 <?php } ?>
